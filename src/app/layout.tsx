1d1340321d43681a6fd491915cf705d8c4896b67
J-Naish <mjyrgd2332@i.softbank.jp>
2025-02-07 03:46:34 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import "../../node_modules/meme-system-ui/dist/tailwind.css";
+import "meme-system-ui/tailwind.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
