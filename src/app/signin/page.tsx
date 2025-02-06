6ab1b707c2e722252b008176a05fae5e15437d32
J-Naish <mjyrgd2332@i.softbank.jp>
2025-02-07 07:53:38 +0900

    create signin page

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,55 @@
+import {
+  Input,
+  Button,
+  VStack,
+  PageTitle,
+  ErrorMessage,
+  Label,
+  LabelText
+} from "meme-system-ui";
+
+export default function Page() {
+  return (
+    <main className="w-full h-screen min-h-[640px] bg-surface flex justify-center items-center">
+      <VStack className="gap-16 pb-16">
+        <VStack className="text-foreground-muted items-center">
+          <span className="font-medium">
+            {/* TODO: 実際のものに変更 */}
+            Acme Inc.管理ダッシュボード
+          </span>
+          <PageTitle>
+            ログイン
+          </PageTitle>
+        </VStack>
+        <form className="flex flex-col gap-4 items-center">
+          <VStack className="w-[400px] gap-8">
+            <VStack className="gap-4">
+              <Label>
+                <LabelText>メールアドレス</LabelText>
+                <Input
+                  placeholder="email@example.com"
+                  type="email"
+                />
+              </Label>
+              <Label>
+                <LabelText>パスワード</LabelText>
+                <Input
+                  placeholder="XXXXXXXXXXXXX"
+                  type="password"
+                />
+              </Label>
+            </VStack>
+            <Button>
+              ログイン
+            </Button>
+          </VStack>
+          <div className="h-4">
+            {/* <ErrorMessage>
+              Here is
+            </ErrorMessage> */}
+          </div>
+        </form>
+      </VStack>
+    </main>
+  );
+}
