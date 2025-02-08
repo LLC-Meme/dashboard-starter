39511e95331b0d9d62fd619f7c5d6af8283c18c0
J-Naish <mjyrgd2332@i.softbank.jp>
2025-02-08 21:52:54 +0900

    create breadcrumb

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import { PageTitle } from "meme-system-ui/components";
+import Breadcrumb from "@/components/UI/breadcrumb";
 
 export const metadata: Metadata = {
   title: "ホーム"
@@ -8,6 +9,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
+      <Breadcrumb links={[]} />
       <PageTitle>
         Acme管理画面
       </PageTitle>
