498e9ea20e33c7c9c4147f0748a42485a373578d
J-Naish <mjyrgd2332@i.softbank.jp>
2025-02-08 15:11:41 +0900

    create a new auth actions of supabase

--- src/actions/auth.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-export const signin = async() => {
-  // TODO: ログイン機能の実装
-};
-
-export const signout = async() => {
-  // TODO: ログアウト機能の実装
-};
---
+++ src/app/actions/auth.ts
@@ -0,0 +1,63 @@
+"use server";
+
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { createServerClient } from "@/lib/supabase/createServerClient";
+import { encodedRedirect } from "@/lib/utils/redirect";
+
+export const signUpAction = async (formData: FormData) => {
+  const email = formData.get("email")?.toString();
+  const password = formData.get("password")?.toString();
+  const supabase = await createServerClient();
+  const origin = (await headers()).get("origin");
+
+  if (!email || !password) {
+    return encodedRedirect(
+      "error",
+      "/sign-up",
+      "Email and password are required",
+    );
+  }
+
+  const { error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      emailRedirectTo: `${origin}/auth/callback`,
+    },
+  });
+
+  if (error) {
+    console.error(error.code + " " + error.message);
+    return encodedRedirect("error", "/sign-up", error.message);
+  } else {
+    return encodedRedirect(
+      "success",
+      "/sign-up",
+      "サインアップに成功しました",
+    );
+  }
+};
+
+export const signInAction = async (formData: FormData) => {
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+  const supabase = await createServerClient();
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    return encodedRedirect("error", "/sign-in", error.message);
+  }
+
+  return redirect("/protected");
+};
+
+export const signOutAction = async () => {
+  const supabase = await createServerClient();
+  await supabase.auth.signOut();
+  return redirect("/sign-in");
+};
