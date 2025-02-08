c841e9abfcf574c93bd970555949c275874baf07
J-Naish <mjyrgd2332@i.softbank.jp>
2025-02-08 21:48:51 +0900

    create breadcrumb component

---
+++ src/components/UI/breadcrumb.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import {
+  BreadcrumbContainer,
+  BreadcrumbItem,
+  BreadcrumbHome,
+  BreadcrumbSeparator,
+} from "meme-system-ui/components";
+import React from "react";
+
+interface Link {
+  label: string;
+  href: string;
+}
+
+interface BreadcrumbProps {
+  links: Link[];
+}
+
+export default function Breadcrumb({
+  links
+}: BreadcrumbProps) {
+  const pathname = usePathname();
+
+  return (
+    <BreadcrumbContainer>
+      <BreadcrumbHome
+        href="/dashboard"
+        current={pathname === "/dashboard"}
+      />
+      {links.map((link, index) => (
+        <React.Fragment key={index}>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem
+            current={pathname === link.href}
+            asChild
+          >
+            <Link href={link.href}>
+              {link.label}
+            </Link>
+          </BreadcrumbItem>
+        </React.Fragment>
+      ))}
+    </BreadcrumbContainer>
+  );
+}
